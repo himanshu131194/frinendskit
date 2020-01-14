2d9e5e344baf1f86dda36bf6907ea750754f908f
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-01-15 01:03:38 +0530

    added

--- client/components/collection/Models/Uploads.js
+++ client/components/collection/Models/Uploads.js
@@ -3,7 +3,13 @@ import {Redirect} from 'react-router-dom'
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
 
+import PostTitle from './PostTitle';
+
+
 class LoginModel extends Component{
+    state = {
+        upload : 0
+    }
     componentDidMount(){
          
     }
@@ -22,51 +28,37 @@ class LoginModel extends Component{
                                             </span>
                                 </div>
                             </div>
-                            <div className="card-body model-pd">
-                            <div className="login-wrapper">
-                            <div className="form-wrapper">
-                                        <div className="login-form">
-                                        <div className="field button-wrap">
-                                           <div className="control uppercase">
-                                              <a className="button is-solid primary-button raised is-fullwidth uppercase mb-15" href="/api/auth/google">Login with Google </a>
-                                              <button className="button is-solid accent-button raised is-fullwidth uppercase">Login with Facebook</button>
-                                           </div>
-                                        </div>
-                                         
-                                        <div className="field">
-                                             <h3 className="uppercase text-center">OR</h3>
-                                        </div>
+                            <div className="card-body model-pd-16">
+                                    {
+                                       this.state.upload==0
+                                       ?<Fragment>
+                                            <PostTitle/>
+                                        </Fragment>
+                                       :                                    
+                                        <div className="login-wrapper">
+                                            <div className="form-wrapper">
+                                                        <div className="login-form">
 
-                                            <div className="field">
-                                                <div className="control">
-                                                    <input className="input email-input" type="text" placeholder="jennadavis@gmail.com" />
-                                                    <div className="input-icon">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user">
-                                                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                                            <circle cx="12" cy="7" r="4"></circle>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="field">
-                                                <div className="control">
-                                                    <input className="input password-input" type="password" placeholder="●●●●●●●" />
-                                                    <div className="input-icon">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-lock">
-                                                            <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                                                            <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="field">
-                                                <div className="control uppercase">
-                                                    <a className="button loginbutton-wrapper is-fullwidth uppercase">Login</a>
-                                                </div>
+                                                        <div className="field button-wrap">
+                                                        <div className="control uppercase">
+                                                            <button className="button is-solid accent-button raised is-fullwidth uppercase">Choose file to upload</button>
+                                                        </div>
+                                                        </div>
+                                                        
+                                                        <div className="field">
+                                                            <h3 className="uppercase text-center">OR</h3>
+                                                        </div>
+
+                                                        <div className="field">
+                                                            <div className="control">
+                                                                <input className="input pl-0 text-center" type="text" placeholder="paste url for picture/video" />
+                                                            </div>
+                                                        </div>
+
+                                                        </div>
                                             </div>
                                         </div>
-                            </div>
-                            </div>
+                                    }
                             </div>
                             <div className="card-footer flex-end">
                                 <div className="button-wrap">
--- client/components/collection/partials/MobileHeader.js
+++ client/components/collection/partials/MobileHeader.js
@@ -64,7 +64,7 @@ class MobileHeader extends Component{
                               : 
                               <Fragment>
                                    <div className="navbar-item">
-                                   <button type="button" className="button is-solid accent-button next-modal raised modal-trigger" data-modal="share-modal">UPLOAD NEW</button>
+                                   <button type="button" className="button is-solid accent-button next-modal raised modal-trigger" data-modal="upload-modal">UPLOAD NEW</button>
                                    </div>
                                    <div id="account-dropdown" className="navbar-item is-account drop-trigger has-caret">
                                             <div className="user-image">
