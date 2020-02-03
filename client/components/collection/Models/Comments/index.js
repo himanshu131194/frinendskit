9bc39e3f794020f3e5e7269f28587c1801c3b296
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-02-03 22:39:52 +0530

    added

--- client/components/collection/Models/Comments/index.js
+++ client/components/collection/Models/Comments/index.js
@@ -117,7 +117,8 @@ export default ()=>{
                                             <div class="media-content">
                                                 <div class="field">
                                                     <p class="control">
-                                                        <textarea class="textarea comment-textarea" rows="5" placeholder="Write a comment..."></textarea>
+                                                       {/* <textarea ref={this.postTitle} className="textarea comment-textarea" rows="1" placeholder="Say something about this ..."></textarea> */}
+                                                        <textarea class="textarea" rows="3" placeholder="Write a comment..."></textarea>
                                                     </p>
                                                 </div>
                                                 <div class="actions">
@@ -128,7 +129,7 @@ export default ()=>{
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-camera"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
                                                         <input type="file"/>
                                                     </div>
-                                                    <a class="button is-solid primary-button raised">Post Comment</a>
+                                                    <a class="button is-solid primary-button raised uppercase">Post Comment</a>
                                                 </div>
                                             </div>
                                         </div>
