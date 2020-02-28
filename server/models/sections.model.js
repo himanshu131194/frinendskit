77d6aa4b0aaea49b0e7ebee926143d54c43c8de7
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-02-29 00:11:25 +0530

    added

--- server/models/sections.model.js
+++ server/models/sections.model.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose'
+import CONFIG from '../../config';
+const {DB} = CONFIG;
+
 const Sections = new mongoose.Schema({
       value: {
            type: String
@@ -8,6 +11,9 @@ const Sections = new mongoose.Schema({
       },
       url: {
             type: String
+      },
+      is_active:{
+      	type: Boolean, default: DB.DEFAULT_TRUE
       },
       created: {
              type: Date,
