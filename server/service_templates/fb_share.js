d66550557c1173ba335c3ef1f90935263d7d9c70
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-05-23 23:15:59 +0530

    added

--- server/service_templates/fb_share.js
+++ server/service_templates/fb_share.js
@@ -3,10 +3,10 @@ export default (post_id, post_title, post_content) => {
     <html>
         <head>
                 <title>Your Website Title</title>
-                <meta property="og:image" content="${post_content}" />
                 <meta property="og:title" content="${post_title}" />
                 <meta property="og:site_name" content="FEELFUNNY" />
                 <meta property="og:description" content="Discover the magic of the internet at feelfunny, a entertainment destination. Lift your spirits with funny jokes, trending memes, entertaining gifs, inspiring stories, and so much more." />
+                <meta property="og:image" content="${post_content}" />
                 <meta property="og:type" content="article" />
         </head>
         <body>
