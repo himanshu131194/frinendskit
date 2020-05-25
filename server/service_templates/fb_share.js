41e9d63c59cf55a92761afce492c3a4f993c166a
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-05-25 23:34:08 +0530

    added

--- server/service_templates/fb_share.js
+++ server/service_templates/fb_share.js
@@ -1,13 +1,12 @@
 export default (post_id, post_title, post_content) => {
-    return `
+    return`
     <html>
         <head>
-                <title>Your Website Title</title>
                 <meta property="og:title" content="${post_title}" />
-<meta property="og:site_name" content="9GAG" />
-<meta property="og:description" content="More memes, funny videos and pics on 9GAG" />
-<meta property="og:type" content="article" />
-<meta property="og:image" content="${post_content}" />
+                <meta property="og:site_name" content="FEELFUNNY" />
+                <meta property="og:description" content="Discover the magic of the internet at feelfunny, a entertainment destination. Lift your spirits with funny jokes, trending memes, entertaining gifs, inspiring stories, and so much more." />
+                <meta property="og:image" content="${post_content}" />
+                <meta property="og:type" content="article" />
         </head>
         <body>
        
