99a2d72b6cc056a0ace694454dc804f9474f682b
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-05-25 23:20:42 +0530

    added

--- server/service_templates/fb_share.js
+++ server/service_templates/fb_share.js
@@ -3,11 +3,12 @@ export default (post_id, post_title, post_content) => {
     <html>
         <head>
                 <title>Your Website Title</title>
+                <meta property="og:title" content="${post_title}" />
 <meta property="og:site_name" content="9GAG" />
 <meta property="og:url" content="http://9gag.com/gag/aAepoGp" />
 <meta property="og:description" content="More memes, funny videos and pics on 9GAG" />
 <meta property="og:type" content="article" />
-<meta property="og:image" content="https://images-cdn.9gag.com/photo/aAepoGp_700b.jpg" />
+<meta property="og:image" content="${post_content}" />
         </head>
         <body>
        
