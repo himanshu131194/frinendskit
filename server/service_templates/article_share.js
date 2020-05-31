730284b178148504f90f1fa906a2a5df2be855e3
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-06-01 00:46:26 +0530

    added

--- server/service_templates/article_share.js
+++ server/service_templates/article_share.js
@@ -2,14 +2,14 @@ export default ({ article_title, article_sitename, article_url, article_descript
     return`
     <html>
         <head>
-            <meta property="og:site_name" content=${article_sitename}/>
-            <meta property="og:url" content=${article_url}/>
-            <meta property="og:title" content=${article_title}/>
-            <meta property="og:description" content=${article_description}/>
+            <meta property="og:site_name" content="${article_sitename}"/>
+            <meta property="og:url" content="${article_url}"/>
+            <meta property="og:title" content="${article_title}"/>
+            <meta property="og:description" content="${article_description}"/>
             <meta property="og:type" content="article"/>
-            <meta property="og:image" content=${article_cover.url}/>
-            <meta property="og:image:width" content=${article_cover.width} />
-            <meta property="og:image:height" content=${article_cover.height} />
+            <meta property="og:image" content="${article_cover.url}"/>
+            <meta property="og:image:width" content="${article_cover.width}" />
+            <meta property="og:image:height" content="${article_cover.height}" />
         </head>
     </html>
     `  
