becf51e4d70d9095b80b9ebf1fb10e143cc188e8
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-06-02 02:28:37 +0530

    added

--- template.js
+++ template.js
@@ -1,4 +1,12 @@
 export default ({ article_title='', article_sitename='', article_url='', article_description='', article_cover='' }) => {
+    let sitename = article_sitename ? article_sitename : '';
+    let url = article_url ? article_url : '';
+    let title = article_title ? article_title : '';
+    let description = article_description ? article_description : '';
+    let cover_url = article_cover? article_cover.url: '';
+    let cover_width = article_cover? article_cover.width: '';
+    let cover_height = article_cover? article_cover.height: '';
+
     return `<!DOCTYPE html>
         <html>
         <head>
@@ -10,14 +18,14 @@ export default ({ article_title='', article_sitename='', article_url='', article
             <meta name=keywords content="funny, image, gif, gifs, memes, jokes, image upload, upload image, lol, humor, vote, comment, share, feelfunny, feelfunny.app, wallpaper" />
             <meta name=description content="Discover the magic of the internet at feelfunny, a entertainment destination. Lift your spirits with funny jokes, trending memes, entertaining gifs, inspiring stories, viral videos, and so much more." />
             <meta name=copyright content="Copyright 2020 feelfunny, Inc." />
-            <meta property="og:site_name" content="${article_sitename? article_sitename: ''}"/>
-            <meta property="og:url" content="${article_url? article_url: ''}"/>
-            <meta property="og:title" content="${article_title? article_title: ''}"/>
-            <meta property="og:description" content="${article_description? article_description: ''}"/>
+            <meta property="og:site_name" content="${sitename}"/>
+            <meta property="og:url" content="${url}"/>
+            <meta property="og:title" content="${title}"/>
+            <meta property="og:description" content="${description}"/>
             <meta property="og:type" content="article"/>
-            <meta property="og:image" content="${article_cover? article_cover.url: ''}"/>
-            <meta property="og:image:width" content="${article_cover? article_cover.width: ''}" />
-            <meta property="og:image:height" content="${article_cover? article_cover.height: ''}" />
+            <meta property="og:image" content="${cover_url}"/>
+            <meta property="og:image:width" content="${cover_width}" />
+            <meta property="og:image:height" content="${cover_height}" />
             <!-- Global site tag (gtag.js) - Google Analytics -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162054083-1"></script>
             <script>
