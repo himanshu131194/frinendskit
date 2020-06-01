55a87fdc9db31ac2b3491b75ff5335797b3aaa4b
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2020-06-01 12:46:22 +0530

    added

--- server/service_templates/article_share.js
+++ server/service_templates/article_share.js
@@ -2,17 +2,26 @@ export default ({ article_title, article_sitename, article_url, article_descript
     return`
     <html>
         <head>
-            <meta property="og:site_name" content="${article_sitename}"/>
-            <meta property="og:url" content="${article_url}"/>
-            <meta property="og:title" content="${article_title}"/>
-            <meta property="og:description" content="${article_description}"/>
+            <meta property="og:site_name" content="Cheezburger"/>
+            <meta property="og:url" content="https://cheezburger.com/11528197/twenty-nine-random-memes-for-people-tired-of-everything"/>
+            <meta property="og:title" content="Twenty-Nine Random Memes For People Tired Of Everything"/>
+            <meta property="og:description" content="You get memes, everyone gets memes!"/>
             <meta property="og:type" content="article"/>
-            <meta property="og:image" content="${article_cover.url}"/>
-            <meta property="og:image:width" content="${article_cover.width}" />
-            <meta property="og:image:height" content="${article_cover.height}" />
+            <meta property="og:image" content="https://i.chzbgr.com/thumb800/11528197/hD7BD6D00/random-motivation-at-2-morning-more-laziness-patch-of-concrete-in-the-middle-of-a-dirt-road"/>
+            <meta property="og:image:width" content="800" />
+            <meta property="og:image:height" content="420" />
         </head>
         <body>
           <a href="https://www.facebook.com/dialog/share?app_id=426940641303361&display=popup&href=https://feelfunny.app/fbshare-article/5ed3f1501374a166a06e306f&redirect_uri=https://feelfunny.app">Goto Logo</a>
         </body>
     </html>`  
 }
+
+            // <meta property="og:site_name" content="${article_sitename}"/>
+            // <meta property="og:url" content="${article_url}"/>
+            // <meta property="og:title" content="${article_title}"/>
+            // <meta property="og:description" content="${article_description}"/>
+            // <meta property="og:type" content="article"/>
+            // <meta property="og:image" content="${article_cover.url}"/>
+            // <meta property="og:image:width" content="${article_cover.width}" />
+            // <meta property="og:image:height" content="${article_cover.height}" />
